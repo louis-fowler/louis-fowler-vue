540926a8f3c34cef1fe8513f387bac0124ee86cc
Louis <louis.fowler@gmail.com>
2020-08-11 14:06:49 +1200

    sorted out image loop

--- src/components/LangCard/LangCard.vue
+++ src/components/LangCard/LangCard.vue
@@ -2,7 +2,7 @@
   <div class="displayCards">
     <div v-for="(lang, index) in langs" :key="index" class="cardContainer">
       <div class="card">
-        <img :src="lang.img" :alt="lang.name" />
+        <img :src="require(`@/assets/img/${lang.img}`)" :alt="lang.name" />
       </div>
       <h3>{{ lang.name }}</h3>
     </div>
@@ -14,7 +14,6 @@ import langArray from "../../assets/data/langArray";
 export default {
   data() {
     return {
-      image: require("../../assets/img/shopify-brands.svg"),
       langs: langArray
     };
   },
